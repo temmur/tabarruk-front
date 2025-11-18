9fd060ed998088806d725e776e4a23322e594777
temmur <143727496+temmur@users.noreply.github.com>
2025-11-18 16:07:16 +0500

    fix issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
     content: [
         "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
+        "./src/**/*.{vue,js,ts,jsx,tsx}"
     ],
+
     theme: {
         extend: {
+            colors: {
+                primary: "#1E40AF",
+                red: {
+                    500: "#F59E0B",
+                },
+                darkBlue: "#001f3f",
+            },
+
             keyframes: {
                 slideIn: {
                     "0%": { opacity: 0, transform: "translateX(50px)" },
@@ -16,5 +26,6 @@ module.exports = {
             },
         },
     },
+
     plugins: [],
-}
+};
